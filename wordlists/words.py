df02aa40b149cfe3e28d96ca88728c21fa6868ca
alexmeinhold <alexander.meinhold@outlook.com>
2019-12-03 20:02:41 +0100

    save words as json file

--- wordlists/words.py
+++ wordlists/words.py
@@ -3,11 +3,14 @@ Worteigenschaften:
 - Länge zwischen 2 und 8 Zeichen
 - keine Bindestriche
 """
+import json
+
 words = []
 
-with open('wortliste.txt') as f:
-    for line in f:
+with open('wortliste.txt') as input_file:
+    for line in input_file:
         if 3 < len(line) < 8 and '-' not in line:
             words.append(line.strip())
 
-print(len(words))
+with open('words.json', 'w', encoding='utf-8') as json_file:
+    json.dump(words, json_file, ensure_ascii=False, indent=4)
