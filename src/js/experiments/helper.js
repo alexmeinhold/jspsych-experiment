68c07e9bfa97b785a24358f3d99d8fbf704ef33d
alexmeinhold <alexander.meinhold@outlook.com>
2020-01-23 00:42:40 +0100

    fix style

--- src/js/experiments/helper.js
+++ src/js/experiments/helper.js
@@ -2,8 +2,8 @@ function permuteWord(word) {
   // https://gist.github.com/customcommander/e9af9da584ff3a33f5ed
   const permutations = [];
 
-  if (word.length == 1) return [word];
-  if (word.length == 2) return [word, word[1] + word[0]];
+  if (word.length === 1) return [word];
+  if (word.length === 2) return [word, word[1] + word[0]];
 
   word.split('').forEach((character, index, array) => {
     const otherChars = [...array];
@@ -13,6 +13,20 @@ function permuteWord(word) {
   });
 
   return permutations;
+}
+
+function hammingDistance(firstWord, secondWord) {
+  if (firstWord.length !== secondWord.length) return -1;
+
+  let index = 0;
+  let distance = 0;
+
+  while (index < firstWord.length) {
+    if (firstWord[index] !== secondWord[index]) distance++;
+    index++;
+  }
+
+  return distance;
 }
 
 export function chooseRandomElement(array) {
@@ -32,21 +46,8 @@ export function randomPermutation(word) {
 export function shuffleArray(array) {
   // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
   for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
-
-function hammingDistance(firstWord, secondWord) {
-  // lets assume both words have the same length
-  let index = 0;
-  let distance = 0;
-
-  while (index < firstWord.length && index < secondWord.length) {
-    if (firstWord[index] !== secondWord[index]) distance++;
-    index++;
-  }
-
-  return distance;
 }
